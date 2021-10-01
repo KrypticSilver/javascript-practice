45e72319dfedff9a050a4116c491e31e3e4d4d82
Toby Hogan <tobyhogan44@gmail.com>
2021-10-01 10:18:59 +0100

    Started working on asynchronisity example.

--- promise.js
+++ promise.js
@@ -1,5 +1,6 @@
 // Standard Promise
 
+
 let myPromise = new Promise(function(myResolve, myReject) {
     const success = true;
 
@@ -12,12 +13,35 @@ let myPromise = new Promise(function(myResolve, myReject) {
     
 });
 
+
+
 myPromise.then(
     function(value) {console.log(value);},
     function(error) {console.log(error);}
 );
 
-// Callbacks using promises
+
+// Callbacks
 // (A callback is a function passed as an argument to another function)
+
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+  }
+  
+  myCalculator(5, 5, myDisplayer);
+
+/* Here, we can control which function is executed second from the
+first function by passing in the second function as an argument. 
+we can do this buy only calling one function outside of their definitions,
+instead of two. */
+
+// Asynchronicity
+
+
 
 
